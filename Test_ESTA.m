f6c37b4a7b4c6e50d7e4496bd5809d165aa483c8
Michael X Zhou <35018333+tiezhongyu2005@users.noreply.github.com>
2026-05-11 09:59:23 +0800

    Add files via upload

---
+++ Test_ESTA.m
@@ -0,0 +1,36 @@
+clear all
+clc
+currentFolder = pwd;
+addpath(genpath(currentFolder))
+warning('off')
+
+
+
+% parameter setting
+funfcn = @Rastrigin;
+n = 50;
+lu = repmat([0;2*n],1,n);
+
+% add shift 
+add_shift = 1;
+if add_shift
+    s = 1:n; 
+else
+    s = 0;
+end
+
+fobj = @(x) funfcn(x-s);
+
+
+tic
+[Best,fBest,hfBest] = ESTA(fobj,n,lu);
+toc
+Best
+fBest
+semilogy(hfBest)
+% numerical gradient
+gradBest = max(abs(num_grad(fobj,Best)))
+
+
+
+
